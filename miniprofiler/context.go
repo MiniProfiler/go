905e64d80729c8d6d14c6f8a81631cdcb0e0a0b4
Craig Peterson <cpeterson@stackoverflow.com>
2016-07-19 11:20:43 -0600

    cleanup

--- miniprofiler/context.go
+++ miniprofiler/context.go
@@ -7,18 +7,21 @@ import (
 	"net/http"
 )
 
-// contextKey can be used to retreive the profiler instance from the request's context
-const contextKey = "MiniProfiler"
+type ctxKey string
 
-// ContextHandler is an alternate handler that passes the profiler on the http request's context rather than as function arguments.
+// contextKey can be used to retreive the profiler instance from the request's context
+const contextKey ctxKey = "miniprofiler"
+
+// ContextHandler is an alternate handler that passes the profiler on the http
+// request's context, rather than as function arguments.
 // This approach is more compatible with standard net/http Handlers.
 type ContextHandler struct {
 	f http.HandlerFunc
-	p *Profile
 }
 
-// NewContextHandler creates a ContextHandler to wrap the given http.HandlerFunc. A profiler will be added to the request Context, and
-// can be retreived with miniprofiler.GetTimer(r)
+// NewContextHandler creates a ContextHandler to wrap the given http.HandlerFunc.
+// A profiler will be added to the request Context, and can be retreived with
+// miniprofiler.GetTimer(r)
 func NewContextHandler(f http.HandlerFunc) http.Handler {
 	return ContextHandler{
 		f: f,
@@ -26,13 +29,14 @@ func NewContextHandler(f http.HandlerFunc) http.Handler {
 }
 
 func (h ContextHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	h.p = NewProfile(w, r, FuncName(h.f))
-	ctx := context.WithValue(r.Context(), contextKey, h.p)
+	p := NewProfile(w, r, FuncName(h.f))
+	ctx := context.WithValue(r.Context(), contextKey, p)
 	h.f(w, r.WithContext(ctx))
-	h.p.Finalize()
+	p.Finalize()
 }
 
-// GetTimer will retreive the timer from the given http request's context. If the request has not been wrapped by a ContextHandler, nil will be returned.
+// GetTimer will retreive the timer from the given http request's context.
+// If the request has not been wrapped by a ContextHandler, nil will be returned.
 func GetTimer(r *http.Request) Timer {
 	val := r.Context().Value(contextKey)
 	if val == nil {
