9d44d1c1d7c641b6fb4389fd570806bb24b2e20d
Craig Peterson <cpeterson@stackoverflow.com>
2017-02-23 17:05:37 -0700

    adding explicitely named context handler option

--- miniprofiler/context.go
+++ miniprofiler/context.go
@@ -16,29 +16,50 @@ const contextKey ctxKey = 0
 // request's context, rather than as function arguments.
 // This approach is more compatible with standard net/http Handlers.
 type ContextHandler struct {
-	f http.HandlerFunc
+	f    http.Handler
+	name string
 }
 
 // NewContextHandler creates a ContextHandler to wrap the given http.HandlerFunc.
 // A profiler will be added to the request Context, and can be retreived with
 // miniprofiler.GetTimer(r)
-func NewContextHandler(f http.HandlerFunc) http.Handler {
+func NewContextHandler(f http.Handler) http.Handler {
 	return ContextHandler{
 		f: f,
 	}
 }
 
+// NewNamedContextHandler creates a ContextHandler to wrap the given http.HandlerFunc.
+// A profiler will be added to the request Context, and can be retreived with
+// miniprofiler.GetTimer(r)
+func NewNamedContextHandler(f http.Handler, name string) http.Handler {
+	return ContextHandler{
+		f:    f,
+		name: name,
+	}
+}
+
 func (h ContextHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	p := NewProfile(w, r, FuncName(h.f))
+	fname := h.name
+	if fname == "" {
+		fname = FuncName(h.f)
+	}
+	p := NewProfile(w, r, fname)
 	ctx := context.WithValue(r.Context(), contextKey, p)
-	h.f(w, r.WithContext(ctx))
+	h.f.ServeHTTP(w, r.WithContext(ctx))
 	p.Finalize()
 }
 
 // GetTimer will retreive the timer from the given http request's context.
 // If the request has not been wrapped by a ContextHandler, nil will be returned.
 func GetTimer(r *http.Request) Timer {
-	val := r.Context().Value(contextKey)
+	return GetTimerFromContext(r.Context())
+}
+
+// GetTimerFromContext will retreive the timer from the given context.
+// If the given context has not been wrapped by a ContextHandler, nil will be returned.
+func GetTimerFromContext(ctx context.Context) Timer {
+	val := ctx.Value(contextKey)
 	if val == nil {
 		return nil
 	}
